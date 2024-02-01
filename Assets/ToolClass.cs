4281e4406e5d15d6de65a7ae83e73273798a420d
Samuel Zhang <sashzhan@ucsc.edu>
2024-01-31 19:39:24 -0800

    Added sample tool script and menu background

---
+++ Assets/ToolClass.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Input;
+
+public class ToolClass : MonoBehaviour
+{
+    public string toolName = "New Item";
+    public string toolDescription = "New Description";
+    public enum Type {Default, Consumable, Permanent}
+    public Type type = Type.Default;
+
+    public GameObject tool;
+    public GameObject currentTool;
+    public int toolNumber = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set current tool to 0
+        currentTool = tool;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetAxis("Mouse ScrollWheel"))
+        {
+            if (Input.GetAxis("Mouse ScrollWheel") > 0)
+            {
+                toolNumber = (toolNumber + 1);
+            }
+            if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            {
+                toolNumber = (toolNumber - 1);
+            }
+        }
+        currentTool = tool;
+    }
+}
