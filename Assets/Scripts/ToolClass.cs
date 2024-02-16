b67955d1d8c493c37d55157effe9cc86633f9f08
aplaxco1 <aplaxco@ucsc.edu>
2024-02-16 14:44:47 -0800

    commenting out tool class stuff for now
    ALSO ADDING CURSOR TEXTURE SO THAT CAN JUST BE PUT IN FOR EACH TOOL YIPEE

--- Assets/Scripts/ToolClass.cs
+++ Assets/Scripts/ToolClass.cs
@@ -5,35 +5,40 @@ using static UnityEngine.Input;
 
 public class ToolClass : MonoBehaviour
 {
-    public string toolName = "New Item";
-    public string toolDescription = "New Description";
-    public enum Type {Default, Consumable, Permanent}
-    public Type type = Type.Default;
+    [Header("Tool Class Properties")]
+    public Texture2D cursorTexture;
 
-    public GameObject tool;
-    public GameObject currentTool;
-    public int toolNumber = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        //set current tool to 0
-        currentTool = tool;
-    }
+    // COMMENTING OUT SOME STUFF SINCE WE ARENT USING IT RIGHT NOW
 
-    // Update is called once per frame
-    void Update()
-    {
-        //if (Input.GetAxis("Mouse ScrollWheel"))
-        {
-            if (Input.GetAxis("Mouse ScrollWheel") > 0)
-            {
-                toolNumber = (toolNumber + 1);
-            }
-            if (Input.GetAxis("Mouse ScrollWheel") < 0)
-            {
-                toolNumber = (toolNumber - 1);
-            }
-        }
-        currentTool = tool;
-    }
+    // public string toolName = "New Item";
+    // public string toolDescription = "New Description";
+    // public enum Type {Default, Consumable, Permanent}
+    // public Type type = Type.Default;
+
+    // public GameObject tool;
+    // public GameObject currentTool;
+    // public int toolNumber = 0;
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     //set current tool to 0
+    //     currentTool = tool;
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     //if (Input.GetAxis("Mouse ScrollWheel"))
+    //     {
+    //         if (Input.GetAxis("Mouse ScrollWheel") > 0)
+    //         {
+    //             toolNumber = (toolNumber + 1);
+    //         }
+    //         if (Input.GetAxis("Mouse ScrollWheel") < 0)
+    //         {
+    //             toolNumber = (toolNumber - 1);
+    //         }
+    //     }
+    //     currentTool = tool;
+    // }
 }
