65ba0c2bc3b621bf0d85f64b373fcc940630213e
Samuel Zhang <sashzhan@ucsc.edu>
2024-02-22 21:57:39 -0800

    Added 2 minute timer to gameplay

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float timeValue = 120;
+    public TMP_Text timerText;
+
+   
+    void Update()
+    {
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 0;
+        }
+
+        DisplayTime(timeValue);
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        if(timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
+        }
+        else if (timeToDisplay > 0)
+        {
+            timeToDisplay += 1;
+        }
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
