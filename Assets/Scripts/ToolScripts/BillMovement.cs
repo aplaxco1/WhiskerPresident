2b59d109c252432b985c40f2691270cbca84269a
aplaxco1 <aplaxco@ucsc.edu>
2024-02-23 19:37:06 -0800

    fixed object outlining

--- Assets/Scripts/ToolScripts/BillMovement.cs
+++ Assets/Scripts/ToolScripts/BillMovement.cs
@@ -35,7 +35,6 @@ public class BillMovement : ToolClass
             if (Physics.Raycast(ray, out hit)) {
                 if (hit.collider.gameObject.CompareTag("Stack") && !billOut && !currBill) {
                     currBill = Instantiate(billPrefab, stackPosition, Quaternion.identity);
-                    // add outline to currBill HERE
                     currBill.GetComponentInChildren<Renderer>().material.SetFloat("_Highlighted", 1);
                     StartCoroutine(moveBill(billPosition, false));
                 }
@@ -76,27 +75,21 @@ public class BillMovement : ToolClass
 
     public void addObjectHighlighting() {
         GameObject stack = GameObject.FindGameObjectWithTag("Stack");
-        // add outline to stack.transform.parent.gameObject HERE
         stack.GetComponentInParent<Renderer>().material.SetFloat("_Highlighted", 1);
         if (billOut) {
-            // add outline to currBill HERE
             currBill.GetComponentInChildren<Renderer>().material.SetFloat("_Highlighted", 1);
         }
         GameObject organizer = GameObject.FindGameObjectWithTag("Organizer");
-        // add outline to organizer.transform.parent.gameObject HERE
         organizer.GetComponentInParent<Renderer>().material.SetFloat("_Highlighted", 1);
     }
 
     public void removeObjectHighlighting() {
         GameObject stack = GameObject.FindGameObjectWithTag("Stack");
-        // remove outline from stack.transform.parent.gameObject HERE
         stack.GetComponentInParent<Renderer>().material.SetFloat("_Highlighted", 0);
         if (billOut) {
-            // remove outline from currBill HERE
-            currBill.GetComponentInParent<Renderer>().material.SetFloat("_Highlighted", 0);
+            currBill.GetComponentInChildren<Renderer>().material.SetFloat("_Highlighted", 0);
         }
         GameObject organizer = GameObject.FindGameObjectWithTag("Organizer");
-        // remove outline from organizer.transform.parent.gameObject HERE
         organizer.GetComponentInParent<Renderer>().material.SetFloat("_Highlighted", 0);
     }
 }
