fb9c616991d77fdbca2ae516856f35a1e833ea8f
Jasper Jas <justinph2013@gmail.com>
2024-05-10 12:45:46 -0700

    continue building out day system

--- Assets/Scripts/SceneScripts/DayInfo.cs
+++ Assets/Scripts/SceneScripts/DayInfo.cs
@@ -13,7 +13,22 @@ public class DayInfo {
     public int statA;
     public int statB;
     public int statC;
+    
     public DayInfo() {
 
+    }
+
+    public string ConvertToString()
+    {
+        string ret = "";
+        ret += "Day: " + day + "\n";
+        ret += "Impeached: " + impeached + "\n";
+        ret += "sinkA: " + sinkA + "\n";
+        ret += "sinkB: " + sinkB + "\n";
+        ret += "sinkC: " + sinkC + "\n";
+        ret += "statA: " + statA + "\n";
+        ret += "statB: " + statB + "\n";
+        ret += "statC: " + statC + "\n";
+        return ret;
     }
 }
