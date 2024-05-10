1519adb827ba375392c284a53740e4a139945d49
dexterxn <dexterxn@gmail.com>
2024-05-10 10:48:51 -0700

    adding phone shaking

--- Assets/Scripts/DistractionScripts/TelephoneDistraction.cs
+++ Assets/Scripts/DistractionScripts/TelephoneDistraction.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TelephoneDistraction : DistractionClass
 {
 
     public AudioSource ringSource;
+    // following is for the shaking
+    private Vector3 startPosition;
+    private Vector3 randomPosition;
+    [Range(0f, 0.1f)]
+    private float phone_vibrate_distance = 0.005f; // change this variable to increase amount of shaking!
+    [Range(0f, 2f)]
+    public float _time = 0.2f;
+    [Range(0f, 0.1f)]
+    public float _delayBetweenShakes = 0.1f;
+
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +27,9 @@ public class TelephoneDistraction : DistractionClass
         maxTime = 50;
         nextEvent = Random.Range(minTime, maxTime);
         distractionPosition = transform.position;
+        startPosition = transform.position; // the og spot of the phone before it starts shaking. 
+
+
     }
 
     // Update is called once per frame
@@ -26,7 +42,33 @@ public class TelephoneDistraction : DistractionClass
 
         if (isActive) {
             distractionEvent();
+            
+            StartCoroutine(Shake());
+            
         }
+    }
+    private IEnumerator Shake()
+    {
+        Debug.Log("TRYNA SHAKE: " + phone_vibrate_distance);
+        while (isActive)
+        {
+            randomPosition = startPosition + (Random.insideUnitSphere * phone_vibrate_distance);
+
+            transform.position = randomPosition;
+            
+
+            if (_delayBetweenShakes > 0f)
+            {
+                yield return new WaitForSeconds(_delayBetweenShakes);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+
+        transform.position = startPosition;
+
     }
 
     public override void distractionEvent() {
@@ -38,6 +80,7 @@ public class TelephoneDistraction : DistractionClass
         if (checkStop()) {
             ringSource.Stop();
             isActive = false;
+            transform.position = startPosition; // set the phone back to the position it was before it started shaking
             timer = 0;
             nextEvent = Random.Range(minTime, maxTime);
         }
