c34a42481dfde572d65c89c0e8430288e6492534
aplaxco1 <aplaxco@ucsc.edu>
2024-05-18 17:56:02 -0700

    added steps for setting up wii remote

---
+++ Assets/Wiimote/WiimoteReadme.cs
@@ -0,0 +1,26 @@
+// STEPS FOR USING WII REMOTE (NOTE: Its only Setup in Office Scene)
+//
+// 1. Connect Wii Remote to PC/Laptop
+//       On Windows 10/11 Devices:
+//           1) Open Control Panel
+//           2) Go to Sound and Devices -> Add a Device
+//           3) On the Wii Remote, Press the (1) and (2) Buttons to Enter Pairing Mode
+//           4) Connect to the Wii Remote
+//              Note:
+//              - It Should Show Up On the List of Available Devices as Nintendo RVL-CNT-1
+//              - While Connecting, Ignore Pin and Press Next
+//           5) Verify Connection By going to Settings-> Bluetooth and Devices
+// 2. Once Connected to PC/Laptop, Turn on Wii Console with the infared bar sensor plugged in.
+//    - This is so that the IR camera in the Wii Remote can be used to detect the screen location.
+//    - If no infared bar is obtainable, theoretically, you can instead set up two heat sources (like candles) 
+//      to act as the infared sensor, but I have not verified if this actually works.
+// 3. Open the Unity Project and Go to Office Scene
+// 4. Play the Office Scene 
+//    - If The 1st and 4th light on the Wii Remote turn on, this means it connected correctly.
+//    - If this does not occur, close the Unity Editor and Reconnect the Wii Remote to try again.
+//
+// 
+// CONTROLS
+// - Hold (A) Button to Turn on Laser.
+// - Move Wii Remote to Move Laser (Press (Home) Button to Re-calibrate the Remote).
+// - Press (B) Button to Turn off Distractions.
