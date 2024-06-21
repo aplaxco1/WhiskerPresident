ceed29f4bb336fb45ad8c3a0c1e950ffd3af90a5
aplaxco1 <aplaxco@ucsc.edu>
2024-06-21 13:44:43 -0700

    he blink :3c

--- Assets/Scripts/CatScripts/FacialExpresions.cs
+++ Assets/Scripts/CatScripts/FacialExpresions.cs
@@ -5,26 +5,57 @@ using UnityEngine;
 public class FacialExpresions : MonoBehaviour
 {
 
+    [Header("Facial Expressions")]
     public GameObject catHead;
     public Material idleFace;
+    public Material idleFaceBlink;
+    public Material happyFace;
     public Material frenzyFace;
+
+    private float timeTillBlink;
+    private float blinkTime;
 
     [Header("Distraction Reference")]
     public DistractionManager distractions;
+
+    [Header("Cat Movement Reference")]
+    public CatMovement catMovement;
     
     // Start is called before the first frame update
     void Start()
     {
+        timeTillBlink = 2f;
+        blinkTime = 0.2f;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (distractions.frenzyActive) {
-            if (catHead.GetComponent<MeshRenderer>().material != frenzyFace) { catHead.GetComponent<MeshRenderer>().material = frenzyFace; }
+            catHead.GetComponent<MeshRenderer>().material = frenzyFace;
         }
         else {
-            if (catHead.GetComponent<MeshRenderer>().material != idleFace) { catHead.GetComponent<MeshRenderer>().material = idleFace; }
+            // change back to idle
+            if (timeTillBlink > 0f) {
+                catHead.GetComponent<MeshRenderer>().material = idleFace; 
+            }
+
+            // blinking animation
+            timeTillBlink -= Time.deltaTime;
+            if (timeTillBlink < 0f) {
+                blink();
+            }
+        }
+    }
+
+
+    void blink() {
+        blinkTime -= Time.deltaTime;
+        catHead.GetComponent<MeshRenderer>().material = idleFaceBlink;
+        if (blinkTime < 0f) {
+            timeTillBlink = 2f;
+            blinkTime = 0.2f;
+            catHead.GetComponent<MeshRenderer>().material = idleFace;
         }
     }
 }
