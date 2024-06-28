a4990786b70c9f95705f5ece5b60e1fe6eec7330
aplaxco1 <aplaxco@ucsc.edu>
2024-06-28 16:41:56 -0700

    stat change popups work now

--- Assets/Scripts/BillScripts/StatTextManager.cs
+++ Assets/Scripts/BillScripts/StatTextManager.cs
@@ -12,6 +12,12 @@ public class StatTextManager : MonoBehaviour
     public TMP_Text statAText;
     public TMP_Text statBText;
     public TMP_Text statCText;
+
+    static public bool statChangePopup = false;
+    static public int statAChange, statBChange, statCChange;
+    static public float popupTime = 3f;
+    public List<GameObject> negativePopups = new List<GameObject>();
+    public List<GameObject> positivePopups = new List<GameObject>();
     
     // temporary day info UI
     public TMP_Text dayInfoText;
@@ -41,5 +47,60 @@ public class StatTextManager : MonoBehaviour
         statCText.text = DayManager.Instance.dayInfo.statC.ToString();
         if (DayManager.Instance.dayInfo.day >= 5) { dayInfoText.text = UnityEngine.Localization.Settings.LocalizationSettings.StringDatabase.GetLocalizedString("String Table", "final-day"); }
         else { dayInfoText.text = UnityEngine.Localization.Settings.LocalizationSettings.StringDatabase.GetLocalizedString("String Table", "day") + " " + DayManager.Instance.dayInfo.day; }
+
+        // displayPopups
+        if (negativePopups.Count > 0 && positivePopups.Count > 0) {
+            if (statChangePopup) {
+                displayPopups();
+                Debug.Log("displaying popups");
+            }
+        }
+    }
+
+    static public void initializePopups(StatVector statChanges) {
+        statChangePopup = true;
+        statAChange = statChanges.StatA;
+        statBChange = statChanges.StatB;
+        statCChange = statChanges.StatC;
+        popupTime = 3f;
+    } 
+
+    void displayPopups() {
+        popupTime -= Time.deltaTime;
+
+        if (statAChange < 0) {
+            negativePopups[0].SetActive(true);
+        }
+        if (statAChange > 0) {
+            positivePopups[0].SetActive(true);
+        }
+        if (statBChange < 0) {
+            negativePopups[1].SetActive(true);
+        }
+        if (statBChange > 0) {
+            positivePopups[1].SetActive(true);
+        }
+        if (statCChange < 0) {
+            negativePopups[2].SetActive(true);
+        }
+        if (statCChange > 0) {
+            positivePopups[2].SetActive(true);
+        }
+
+        if (popupTime < 0f) {
+            statChangePopup = false;
+            removePopups();
+        }
+
+    }
+
+    void removePopups() {
+        foreach (GameObject popup in negativePopups) {
+            popup.SetActive(false);
+        }
+
+        foreach (GameObject popup in positivePopups) {
+            popup.SetActive(false);
+        }
     }
 }
